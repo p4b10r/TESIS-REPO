5890fc94f8a152860f182a85feedae85c47d665f
P4b10r <p4b10r@protonmail.com>
2020-05-29 00:03:10 -0400

    Actualización de documento Latex y txt de operaciones impresora API

--- TEST-API/test.py
+++ TEST-API/test.py
@@ -6,24 +6,30 @@ import json
 app=Flask(__name__)
 
 api_token = 'FB44A2D826704E18910B445C30520F6B'
-api_url_base = 'http://192.168.1.95/'
+api_url_base = 'http://192.168.1.95:5000/'
 
-api_url = '{}{}'.format(api_url_base, 'api/connection')
+api_url = '{}{}'.format(api_url_base, '/api/connection')
 
 headers = {
          'Content-Type': 'application/json',
          'X-Api-Key': api_token # the name may vary.  I got it from this doc: http://docs.octoprint.org/en/master/api/job.html
           }
+data1={'status': 'port','status': 'baudrate'}
+
+
+
 data = {
          'command': 'pause', # notice i also removed the " inside the strings
          'action': 'pause'
 
           }
 
-print(api_url)
-response = requests.get(api_url, headers=headers, json=data)
 
-print(response.json())
+print(api_url)
+#response = requests.get(api_url, headers=headers, json=data)
+response2=request.get(api_url, headers= headers, json=data1)
+
+print(response2.json())
 
 
 if __name__=='__main__':
--- app/app/views.py
+++ app/app/views.py
@@ -1,12 +1,44 @@
-from app import app
+from flask import Flask, jsonify#, request
+import requests
+import json
+#from app import app
+import urllib3
+from time import sleep
+
+
+
+app=Flask(__name__)
+
 
 @app.route('/')
+
+
 def Index():
-    return 'Index'
+
+    return 'este es el index'
+
 
 @app.route('/centro')
+
 def CentroImpresion():
-    return 'este es el centro de impresion'
+
+
+
+    api_token = 'FB44A2D826704E18910B445C30520F6B'
+    api_url_base = 'http://192.168.1.95:5000/'
+
+    api_url = '{}{}'.format(api_url_base, 'api/connection')
+
+    PARAMS = {'Content-Type': 'application/json','X-Api-Key': api_token} # the name may vary.  I got it from this doc: http://docs.octoprint.org/en/master/api/job.html
+
+
+    data={'status':'current'}
+
+    response=requests.get(api_url, headers=PARAMS, json=data)
+    sleep(1)#
+
+
+    return json(response)
 
 @app.route('/mtto')
 def CentroMtto():
@@ -15,3 +47,8 @@ def CentroMtto():
 @app.route('/produccion')
 def CentroProd():
     return 'este es el centro de produccion'
+
+
+if __name__=='__main__':
+
+    app.run(host='192.168.1.95',port=4000, debug=True)
